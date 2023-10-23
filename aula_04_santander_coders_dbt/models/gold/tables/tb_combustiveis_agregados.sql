73ca0f4516bbca608d3e670eec7c70be679aceca
Juan Domingos <juan.domingos@brazillisit.com.br>
2023-10-23 20:04:44 -0300

    Feat: Exercicio Aula 7

--- aula_04_santander_coders_dbt/models/gold/tables/tb_combustiveis_agregados.sql
+++ aula_04_santander_coders_dbt/models/gold/tables/tb_combustiveis_agregados.sql
@@ -1,4 +1,11 @@
-  SELECT ref.municipio,
+{{
+  config(
+    materialized="table"
+  )
+}}
+
+  SELECT {{ dbt_utils.generate_surrogate_key(['municipio', 'sigla_estado']) }} as id,
+         ref.municipio,
          ref.sigla_estado,
          round(AVG(ref.valor_de_venda), 2) AS media
     FROM {{ ref('tb_precos_combustiveis_refinado') }} ref
